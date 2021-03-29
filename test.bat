1c11d5cc6fb79d8de26d799d8bd335d8952aeb6e
Behruz Veliyev <62702293+behruz-code@users.noreply.github.com>
2021-03-29 11:06:53 +0400

    Son deyisiklik

---
+++ test.bat
@@ -0,0 +1,8 @@
+cd P:\ProgmatechPhpProject
+git.exe checkout dev
+git init
+git remote add origin https://github.com/behruz-code/ProgmatechPhpProject.git
+git.exe add .
+git.exe commit -am "Son deyisiklik"
+git.exe push
+pause
